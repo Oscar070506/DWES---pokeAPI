aa71d22cba459aa170dc53f1d8b24e9da4af0f8b
Oscar070506 <2006ospi@gmail.com>
2025-11-12 19:12:21 +0100

    Add files via upload

---
+++ Practica-API/Moves.cs
@@ -0,0 +1,13 @@
+public class Moves
+{
+
+    public Move Move { get; set; }
+    public string Name { get; set; }
+    public int Order { get; set; }
+}
+
+public class Move
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
---
+++ Practica-API/PokeAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+class Program
+{
+    static async Task Main()
+    {
+        // Crear el cliente HTTP
+        using (HttpClient client = new HttpClient())
+        {
+            // URL de ejemplo: una API pública gratuita
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            try
+            {
+                // Enviar la petición GET
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+                Todo todo = JsonSerializer.Deserialize<Todo>(content);
+                Console.WriteLine($"Tarea #{todo.Id}: {todo.Title} (Completada:{todo.Completed})");
+
+                Console.WriteLine("Respuesta recibida:");
+                Console.WriteLine(content);
+            }
+
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error en la solicitud:{e.Message}");
+            }
+        }
+    }
+}
+
