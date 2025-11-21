4b74d7643f85872c266269fea18ac5b3145efa01
Oscar070506 <2006ospi@gmail.com>
2025-11-21 19:54:40 +0100

    Update Moves.cs

--- Practica-API/Moves.cs
+++ Practica-API/Moves.cs
@@ -1,13 +1,1 @@
-public class Moves
-{
 
-    public Move Move { get; set; }
-    public string Name { get; set; }
-    public int Order { get; set; }
-}
-
-public class Move
-{
-    public string Name { get; set; }
-    public string Url { get; set; }
-}
